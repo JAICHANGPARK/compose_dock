28be4b3afd64589000cb60f6968ed395d338e9a5
Dreamwalker <aristojeff@gmail.com>
2021-08-15 10:24:21 +0900

    Update CounterApp.kt

--- app/src/main/java/dreamwalker/com/compose_dock/counter/CounterApp.kt
+++ app/src/main/java/dreamwalker/com/compose_dock/counter/CounterApp.kt
@@ -1,12 +1,18 @@
 package dreamwalker.com.compose_dock.counter
 
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 
 @Preview(showBackground = true)
@@ -24,7 +30,9 @@ fun CounterApp() {
             )
         },
         floatingActionButton = {
-            FloatingActionButton(onClick = { /*TODO*/ }) {
+            FloatingActionButton(onClick = {
+                counter.value++
+            }) {
                 Icon(
                     imageVector = Icons.Default.Add,
                     contentDescription = "Add"
@@ -33,10 +41,19 @@ fun CounterApp() {
             }
         },
         content = {
-            Column() {
-                Text(text = "You have pushed the button this many times")
-                Text(text = "4")
-
+            Column(
+                horizontalAlignment = Alignment.CenterHorizontally,
+                verticalArrangement = Arrangement.Center,
+                modifier = Modifier.fillMaxSize()
+            ) {
+                Text(
+                    text = "You have pushed the button this many times",
+                    textAlign = TextAlign.Center
+                )
+                Text(
+                    text = "${counter.value}",
+                    textAlign = TextAlign.Center
+                )
             }
         }
     )
